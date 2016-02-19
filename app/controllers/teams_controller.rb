30f60e4f785edb6ae512bb3a9067b202d1888da4
Nathan Lilienthal <nathan@nixpulvis.com>
2016-02-18 21:53:40 -0500

    Team page working.

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,4 +1,11 @@
 class TeamsController < ApplicationController
+  # GET /courses/:course_id/teams/:id
   def show
+    @course = Course.find(params[:course_id])
+    @team = Team.find(params[:id])
+
+    if @team.users.exclude?(current_user)
+      redirect_to(root_path, alert: "You are not a member of that team.")
+    end
   end
 end
