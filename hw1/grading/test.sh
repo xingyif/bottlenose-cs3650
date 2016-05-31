ce765dd0548a9de911bf6496baef69bc23b4add1
Nathan Lilienthal <nathan@nixpulvis.com>
2016-05-31 15:50:47 -0400

    Adding a simple example of a working grading directory.

---
+++ hw1/grading/test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+OUTPUT=$(cat ../strings.rkt | racket | grep "\"hello world\"")
+
+echo "1..1"
+
+if [ "$OUTPUT" != '' ]; then
+    echo "ok 1 - strings.rkt"
+else
+    echo "not ok 2 - strings.rkt"
+fi
