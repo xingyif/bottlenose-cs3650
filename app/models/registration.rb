24710ad219fff99b2cfe95f5b8ace572e20dee0e
Nathan Lilienthal <nathan@nixpulvis.com>
2016-06-11 17:44:00 -0400

    Add some docs to registations and fix broken methods.

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,22 +1,27 @@
 class Registration < ActiveRecord::Base
+  # The role for a regestration is a way for Bottlenose to allow a single user
+  # to be a staff role for one course, while being a student in another.
+  # Professors have extra privileges over staff.
   enum role: [:student, :staff, :professor]
 
+  # A regestration is join model between a user and a course.
   belongs_to :user
   belongs_to :course
 
-  validates :user_id,   :presence => true
-  validates :course_id, :presence => true
+  # Only one regestration per user per course is allowed.
+  validates :user_id, uniqueness: { scope: :course_id }
 
-  validates :user_id, :uniqueness => { :scope => :course_id }
-
+  # TODO <refactor>: Delete, this looks unused, if only we have a compiler...
   def self.get(c_id, u_id)
     Registration.find_by_course_id_and_user_id(c_id, u_id)
   end
 
+  # Return the submissions to the course the user is registered for.
   def submissions
-    Submission.where(user_id: user.id, course_id: course.id)
+    user.submissions.select { |s| s.course == course }
   end
 
+  # TODO <refactor>: Is this used?
   def best_subs
     Assignment.where(course_id: course_id).map do |aa|
       aa.main_submission_for(user)
@@ -25,6 +30,8 @@ class Registration < ActiveRecord::Base
     end
   end
 
+  # Return the total score for the student in the course. This value is
+  # meaningless for staff and professors. The value is a percent.
   def total_score
     total = 0.0
 
