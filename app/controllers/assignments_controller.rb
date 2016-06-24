1f52906203bb4a557a33c4f5f519302d79cb539d
Nathan Lilienthal <nathan@nixpulvis.com>
2016-06-24 17:15:06 -0400

    Remove all staff links.

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -26,6 +26,7 @@ class AssignmentsController < CoursesController
   end
 
   def edit
+    @assignment = Assignment.find(params[:id])
   end
 
   def create
--- app/views/staff/main/dashboard.html.erb
+++
@@ -1,1 +0,0 @@
-<h1>Staff Dashboard</h1>
