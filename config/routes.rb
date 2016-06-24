f63994e2c4ed6ff3dc82e701028bd4df3dd639d2
Nathan Lilienthal <nathan@nixpulvis.com>
2016-06-24 14:36:56 -0400

    Comment out staff routes, will be deleted later.

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ Bottlenose::Application.routes.draw do
   # Using devise for user auth.
   devise_for :users
 
-  # Student routes.
-  #################
-
   root to: "main#home"
   get "about" => "main#about"
 
@@ -35,41 +32,41 @@ Bottlenose::Application.routes.draw do
     end
   end
 
-  # Staff routes.
-  ###############
-
-  namespace :staff do
-    root to: "main#dashboard"
-
-    resource :settings, only: [:edit, :update]
-    resources :users do
-      collection do
-        post :stop_impersonating
-      end
-      member do
-        post :impersonate
-      end
-    end
-    resources :terms
-    resources :courses do
-      delete 'reg_requests/:id/accept', to: 'reg_requests#accept', as: 'reg_request_accept'
-      delete 'reg_requests/:id/reject', to: 'reg_requests#reject', as: 'reg_request_reject'
-      resources :registrations, except: [:edit, :update] do
-        collection do
-          post :bulk
-        end
-        member do
-          post :toggle
-        end
-      end
-      resources :teams do
-        member do
-          patch :disolve
-        end
-      end
-      resources :assignments do
-        resources :submissions
-      end
-    end
-  end
+  # # Staff routes.
+  # ###############
+  #
+  # namespace :staff do
+  #   root to: "main#dashboard"
+  #
+  #   resource :settings, only: [:edit, :update]
+  #   resources :users do
+  #     collection do
+  #       post :stop_impersonating
+  #     end
+  #     member do
+  #       post :impersonate
+  #     end
+  #   end
+  #   resources :terms
+  #   resources :courses do
+  #     delete 'reg_requests/:id/accept', to: 'reg_requests#accept', as: 'reg_request_accept'
+  #     delete 'reg_requests/:id/reject', to: 'reg_requests#reject', as: 'reg_request_reject'
+  #     resources :registrations, except: [:edit, :update] do
+  #       collection do
+  #         post :bulk
+  #       end
+  #       member do
+  #         post :toggle
+  #       end
+  #     end
+  #     resources :teams do
+  #       member do
+  #         patch :disolve
+  #       end
+  #     end
+  #     resources :assignments do
+  #       resources :submissions
+  #     end
+  #   end
+  # end
 end
