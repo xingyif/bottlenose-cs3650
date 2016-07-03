4ba825ca96d952b3522dcc421785bcad0a43538c
Ben Lerner <blerner@ccs.neu.edu>
2016-07-03 13:21:03 -0400

    Inlining the devise login form into the homepage

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -10,6 +10,21 @@ class MainController < ApplicationController
     end
   end
 
+  def resource_name
+    :user
+  end
+  helper_method :resource_name
+
+  def resource
+    @resource ||= User.new
+  end
+  helper_method :resource
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+  helper_method :devise_mapping
+
   # GET /about
   def about
   end
