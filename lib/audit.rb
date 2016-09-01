871110d2d887fca14b9145433de20c3dffb6f1f0
Ben Lerner <blerner@ccs.neu.edu>
2016-09-01 18:28:15 -0400

    .

--- lib/audit.rb
+++ lib/audit.rb
@@ -1,6 +1,6 @@
 class Audit
+  @@log = Logger.new(File.open(Rails.root.join("log", "audit-#{Rails.env}.log"), File::WRONLY | File::APPEND))
   def self.log(msg)
-    @@log ||= Logger.new(File.open(Rails.root.join("log", "audit-#{Rails.env}.log"), "w"))
     @@log.info("#{Time.now}: #{msg}")
   end
 end
