f3057b2c70ff40e4bed2d114758c7d5cf6339cf9
Ben Lerner <blerner@ccs.neu.edu>
2016-08-31 21:29:19 -0400

    better JS validation of arrow keys and preventing pasting into NUID field; should this be rolled out to all numeric fields?

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -148,6 +148,7 @@ function activateSpinner(obj, options) {
     validateNumericInput(e);
     if (e.key === "ArrowUp") { increment(); return; }
     if (e.key === "ArrowDown") { decrement(); return; }
+    if (e.key === "ArrowLeft" || e.key === "ArrowRight") { return; }
     var curVal = $(this).val();
     var newVal = curVal.slice(0, this.selectionStart) + e.key + curVal.slice(this.selectionEnd, curVal.length);
     newVal = parseFloat(newVal, 10);
